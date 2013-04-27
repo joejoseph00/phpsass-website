3bfd7453082f797a4064f5541f4a0c9d64b6a273
root <root@richthegeek.co.uk>
2013-04-27 21:24:52 +0100

    Update git-hook.php

--- git-hook.php
+++ git-hook.php
@@ -5,19 +5,19 @@ file_put_contents("git-hook.txt", time());
 # right format...
 if (!isset($_REQUEST['payload'])) {
   file_put_contents("git-hook.txt", "NO PAYLOAD");
-  exit 1;
+  exit;
 }
 
 try {
   $payload = json_decode($_REQUEST['payload']);
-} catch ($e) {
+} catch (Exception $e) {
   file_put_contents("git-hook.txt", "BAD PAYLOAD JSON");
-  exit 2;
+  exit;
 }
 
 if (substr($payload->repository->name, 0, 7) != 'phpsass') {
   file_put_contents("git-hook.txt", "BAD REPO NAME");
-  exit 2;
+  exit;
 }
 
 # get the info from github
@@ -25,14 +25,14 @@ $info = file_get_contents("https://api.github.com/repos/richthegeek/phpsass-webs
 
 try {
   $json = json_decode($info);
-} catch ($e) {
+} catch (Exception $e) {
   file_put_contents("git-hook.txt", "BAD INFO");
-  exit 3;
+  exit;
 }
 
 if (!isset($json->updated_at)) {
   file_put_contents("git-hook.txt", "BAD TIME");
-  exit 4;
+  exit;
 }
 
 $updated = strtotime($json->updated_at);
@@ -41,13 +41,13 @@ $diff = abs(time() - $updated);
 
 if ($diff > 600) {
   file_put_contents("git-hook.txt", "BAD DIFF");
-  exit 5;
+  exit;
 }
 
 # should be good to pull now.
 if ($payload->repository->name == 'phpsass-website') {
-  `git pull >> git-hook.txt`
+  `git pull >> git-hook.txt`;
 }
 else {
-  `git submodule update >> git-hook.txt`
+  `git submodule update >> git-hook.txt`;
 }
